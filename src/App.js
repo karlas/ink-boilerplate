280c7524540d94868e2bcb47fc059a4abf2b3372
karlas <carles.pages.rozas@gmail.com>
2020-03-25 12:58:27 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import { Color } from 'ink'
+
+export default () => <Color blue>Hello!</Color>
---
+++ tests/dummy.test.js
@@ -0,0 +1,6 @@
+describe('should work', () => {
+  it('it works', async () => {
+    const dummy = () => 69
+    expect(dummy()).toEqual(69)
+  })
+})
